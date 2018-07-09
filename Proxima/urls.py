81cff94afd54279edaa00bd97fea2ed524ef1e4b
Unknown <daniel.kamali@yahoo.com>
2018-07-09 13:09:23 +0430

    urls renewd

--- Proxima/urls.py
+++ Proxima/urls.py
@@ -29,5 +29,17 @@ urlpatterns = [
     path('book/', ServicePageController.ServicePageController.Book, name='book'),
     path('comment/<int:id>/', BusinessPageController.comment, name='comment'),
     path('createBusiness/<int:id>',AccountPageController.createBusiness,name='createBusiness'),
-    path('test',views.test)
+    path('test',views.test),
+    path('dashboard/<int:business_id>',dashboardController.render_dashboard , name='dashboard'),
+    path('changePhoto/<int:id>',dashboardController.changePhoto,name='changePhoto'),
+    path('test/<int:id>',views.imagetest,name='imagetest'),
+    path('addService/<int:business_id>',dashboardController.addService,name="addService"),
+    path('editService/<int:service_id>',editServiceController.Render,name='editServicePage') ,
+    path('addBusiness',addAndDeleteBusinessController.addBusiness,name = 'addBusiness'),
+    path('deleteSans/<int:sans_id>/<int:service_id>',editServiceController.deleteSans,name='deleteSans'),
+    path('buildTimeTable/<int:service_id>', editServiceController.buildTimeTable , name = 'buildTimeTable')
+
 ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
